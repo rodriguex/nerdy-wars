979e334d61c13cb9f401c044010bac0a9741be4d
Gustavo Rodrigues <gustardn@gmail.com>
2025-06-16 23:54:26 -0300

    Basic game running

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,128 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const questions = [
+    {
+      id: 1,
+      question: "What's SQL?",
+      alternatives: [
+        {
+          id: 1,
+          text: "Programming language",
+          option: "A",
+        },
+        {
+          id: 2,
+          text: "Data structure",
+          option: "B",
+        },
+        {
+          id: 3,
+          text: "Your hot mama",
+          option: "C",
+        },
+      ],
+      answer: "A",
+    },
+    {
+      id: 2,
+      question: "What's PHP?",
+      alternatives: [
+        {
+          id: 1,
+          text: "A programming language",
+          option: "A",
+        },
+        {
+          id: 2,
+          text: "A data structure",
+          option: "B",
+        },
+        {
+          id: 3,
+          text: "Your hot mama",
+          option: "C",
+        },
+      ],
+      answer: "A",
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [isGameFinished, setIsGameFinished] = useState(false);
+  const [answerTime, setAnswerTime] = useState(5);
+
+  useEffect(() => {
+    if (isGameFinished) return;
+
+    const interval = setInterval(() => {
+      setAnswerTime((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isGameFinished, currentQuestion]);
+
+  useEffect(() => {
+    if (answerTime === 0) {
+      goToNextQuestion();
+      setAnswerTime(5);
+    }
+  }, [answerTime]);
+
+  function goToNextQuestion() {
+    const nextQuestionIdx =
+      questions.findIndex((item) => item.id === currentQuestion.id) + 1;
+
+    if (!questions[nextQuestionIdx]) {
+      setIsGameFinished(true);
+      return;
+    }
+
+    setSelectedAnswer("");
+    setAnswerTime(5);
+    setCurrentQuestion(
+      questions[
+        questions.findIndex((item) => item.id === currentQuestion.id) + 1
+      ]
+    );
+  }
+
+  return (
+    <div className="flex flex-col h-screen justify-center items-center text-xl w-[400px] mx-auto">
+      <div className="w-full">
+        {isGameFinished ? (
+          <h1 className="font-bold text-3xl text-center">Game finished</h1>
+        ) : (
+          <>
+            <div className="flex items-center justify-between">
+              <span>{currentQuestion.question}</span>
+              <span className="font-bold text-2xl">{answerTime}</span>
+            </div>
+            <div className="mt-6 flex flex-col gap-3">
+              {currentQuestion.alternatives.map((alternative) => (
+                <span
+                  key={alternative.id}
+                  className={`border p-4 rounded-lg cursor-pointer ${
+                    selectedAnswer === alternative.option
+                      ? "bg-black text-white"
+                      : "bg-white text-black"
+                  }`}
+                  onClick={() => setSelectedAnswer(alternative.option)}
+                >{`${alternative.option} - ${alternative.text}`}</span>
+              ))}
+            </div>
+            <button
+              className="mt-10 border font-bold text-xl p-3 rounded-lg w-full hover:bg-black hover:text-white cursor-pointer"
+              onClick={goToNextQuestion}
+            >
+              Next
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
